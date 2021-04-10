6252e3fef28fd16b658aebaaf4c17bba4573995d
kevleop <kvalverde@unsa.edu.pe>
2021-04-09 19:38:11 -0500

    Finalizacion de chat usando sockets

--- semana 11/backend/src/config/server.js
+++ semana 11/backend/src/config/server.js
@@ -23,6 +23,7 @@ export default class ServidorSocket {
     const mensajes = [];
     this.io.on("connect", (cliente) => {
       // console.log(cliente);
+      this.io.emit("lista-usuarios", usuarios);
       console.log(`Se conecto el cliente ${cliente.id}`);
       cliente.on("disconnect", (motivo) => {
         console.log(`Se desconecto el cliente ${cliente.id}`);
@@ -33,12 +34,23 @@ export default class ServidorSocket {
       });
 
       cliente.on("configurar-cliente", (nombre) => {
-        console.log(nombre);
         usuarios.push({
           id: cliente.id,
           nombre,
         });
         this.io.emit("lista-usuarios", usuarios);
+        // cliente.broadcast.emit("lista-usuarios", usuarios);
+      });
+      cliente.on("mensaje", (mensaje) => {
+        const usuario = usuarios.filter(
+          (usuario) => usuario.id === cliente.id
+        )[0];
+        console.log(usuario);
+        mensajes.push({
+          cliente: usuario.nombre,
+          mensaje,
+        });
+        this.io.emit("lista-mensajes", mensajes);
       });
     });
   }
--- semana 11/frontend/main.js
+++ semana 11/frontend/main.js
@@ -3,7 +3,7 @@ const nombre = document.getElementById("nombre");
 const ingresar = document.getElementById("ingresar");
 const listaUsuarios = document.getElementById("lista-usuarios");
 const mensaje = document.getElementById("mensaje");
-const listaMensaje = document.getElementById("lista-mensajes");
+const listaMensajes = document.getElementById("lista-mensajes");
 
 // connect: sirve para conectarnos al socket (y el backend recibira la notificaion de la nueva conexion)
 // el metodo on sirve para mandar algo al back
@@ -12,11 +12,25 @@ socket.on("connect", () => {
 });
 
 // el metodo emit sirve para enviar algo al back mediente el nombre de su socket
+
 ingresar.addEventListener("click", (e) => {
   e.preventDefault();
-  socket.emit("configurar-cliente", nombre.value);
-  ingresar.disabled = true;
-  nombre.disabled = true;
+  const classbtn = ingresar.classList;
+  if (classbtn.contains("btn-danger")) {
+    socket.disconnect().connect();
+    // ^^ sirve para hacer un hard-reset del client del socket (expulsa al cliente)
+    // y automaticamente se intenta reconectar
+    ingresar.innerText = "Ingresar al chat";
+    ingresar.className = "btn btn-block btn-success";
+    nombre.disabled = false;
+    nombre.value = "";
+  } else {
+    socket.emit("configurar-cliente", nombre.value);
+    // ingresar.disabled = true;
+    nombre.disabled = true;
+    ingresar.className = "btn btn-danger btn-block";
+    ingresar.innerText = "Salir del chat";
+  }
 });
 
 socket.on("lista-usuarios", (usuarios) => {
@@ -28,4 +42,23 @@ socket.on("lista-usuarios", (usuarios) => {
     usuarioLi.innerText = usuarios[key].nombre;
     listaUsuarios.appendChild(usuarioLi);
   }
+});
+
+mensaje.addEventListener("keyup", (e) => {
+  if (e.key === "Enter") {
+    socket.emit("mensaje", mensaje.value);
+    mensaje.value = "";
+  }
+});
+
+socket.on("lista-mensajes", (mensajes) => {
+  console.log(mensajes);
+
+  listaMensajes.innerHTML = "";
+  mensajes.forEach((mensaje) => {
+    const mensajeLi = document.createElement("li");
+    mensajeLi.className = "list-group-item";
+    mensajeLi.innerText = `${mensaje.cliente} dice: ${mensaje.mensaje}`;
+    listaMensajes.appendChild(mensajeLi);
+  });
 });
